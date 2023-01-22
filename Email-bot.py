8188793f18a2b38b4a5f8072d4fdf6df1b3654f2
nitin <nitinpriyadarshi70@gmail.com>
2023-01-22 16:42:14 +0530

    Update Email-bot.py

--- Email-bot.py
+++ Email-bot.py
@@ -39,3 +39,37 @@ def send_email(receiver, subject, message):
     email['Subject'] = subject
     email.set_content(message)
     server.send_message(email)
+
+
+
+
+email_list = {
+    'dude': 'COOL_DUDE_EMAIL',
+    'nitin': 'nitinpriyadarshi70@gmail.com',
+    'pink': 'jennie@blackpink.com',
+    'lisa': 'lisa@blackpink.com',
+    'irene': 'irene@redvelvet.com'
+}
+
+
+def get_email_info():
+    name = time.sleep(2)
+    talk('Tell me name of person you want to send email')
+    name = get_info()
+
+    receiver = email_list[name]
+    print(receiver)
+    talk('What is the subject of your email?')
+    subject = get_info()
+    talk('Tell me the text in your email')
+    message = get_info()
+    send_email(receiver, subject, message)
+    talk(' Your email is sent')
+    talk('Do you want to send more email?')
+    send_more = get_info()
+    if 'yes' in send_more:
+        get_email_info()
+
+
+
+get_email_info()
